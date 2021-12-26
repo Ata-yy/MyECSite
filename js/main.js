3b936014983b0323d66b22c8a41af2ce6d5cfc67
Ata-yy <93063275+Ata-yy@users.noreply.github.com>
2021-12-26 21:20:20 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,52 +1,24 @@
 'use strict';
 
 {
-  // Intersection Observer API
-  // IntersectionObserver
-  function inViewcallback(entries, obs) {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) {
-        return;
-      }
 
-      entry.target.classList.add('appear');
-      obs.unobserve(entry.target);
-    });
+  function play () {
+    setTimeout(() => {
+      images[currentIndex].classList.remove('current');
+      currentIndex++;
+      if(currentIndex > images.length - 1) {
+        currentIndex = 0;
+      }
+      images[currentIndex].classList.add('current');
+      play();
+    }, 5000);  
   }
 
-  function onScrollcallback(entries) {
-    entries.forEach(entry => {
-      if(!entry.isIntersecting) {
-        header.classList.add('scrolled');
-        toTop.classList.add('scrolled');
-      } else {
-        header.classList.remove('scrolled');
-        toTop.classList.remove('scrolled');
-      }
-    });
-  }
+  const images = document.querySelectorAll('.hero img');
+  let currentIndex = 0;
 
-  const inViewobserver = new IntersectionObserver(inViewcallback, {threshold: 0.2});
-
-  document.querySelectorAll('.animate').forEach(el => {
-    inViewobserver.observe(el);
-  });
+  play();
 
 
-
-  // onScrollobserver
-  const onScrollobserver = new IntersectionObserver(onScrollcallback);
-  onScrollobserver.observe(document.getElementById('target'));
-  const header = document.querySelector('header');
-  const toTop = document.getElementById('to_top');
-
-  // click
-  toTop.addEventListener('click', e => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  });
 
 }
